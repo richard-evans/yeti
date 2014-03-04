7e9ea685762d92d16a807a20e1e8c87c01ed253a
Richard Evans <richard.evans@york.ac.uk>
2014-03-04 21:44:42 +0000

    Added placeholder initialise function

---
+++ src/initialise.cpp
@@ -0,0 +1,35 @@
+//-----------------------------------------------------------------------------
+//
+// This source file is part of the YETI open source package under the
+// BSD (2-clause) licence (see LICENCE file for details).
+//
+// (c) H Hathrell, S Jenkins, R F L Evans 2014. All rights reserved.
+//
+//-----------------------------------------------------------------------------
+
+// System headers
+#include <iostream>
+
+// Program headers
+#include "yeti.hpp"
+
+namespace yeti{
+   
+//-----------------------------------------------------------------------------
+// Function to initialise system parameters and objects
+//-----------------------------------------------------------------------------
+void initialise(){
+      
+   
+  // process input file to get simulation and microscope parameters
+   
+   
+  // process objects file to generate shapes 
+  yeti::create();
+   
+  return;
+      
+}
+
+   
+}
